85f43f1c987abc1551571bfcc9ff8933625e7d97
Ángel Soliño <asolfre@gmail.com>
2018-08-30 00:22:59 +0200

    Fix ruleset route

--- pmd.sh
+++ pmd.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cd $HOME
 wget https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.6.0/pmd-bin-6.6.0.zip
 unzip pmd-bin-6.6.0.zip
-alias pmd="$HOME/pmd-bin-6.6.0/bin/run.sh pmd"
-pmd -d . -R ruleset.xml -f xml
+pmd-bin-6.6.0/bin/run.sh pmd -d . -R ruleset.xml -f xml -reportfile pmd-report.xml -l apex
