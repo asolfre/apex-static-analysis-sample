319580192404132b1fb705ad3a881b35f0c0ade4
Ángel Soliño <asolfre@gmail.com>
2018-08-30 00:37:15 +0200

    Switch ruleset
    Now using https://github.com/ChuckJonas/vscode-apex-pmd/blob/master/rulesets/apex_ruleset.xml instead of https://github.com/pmd/pmd/blob/master/pmd-apex/src/main/resources/rulesets/apex/ruleset.xml

--- pmd.sh
+++ pmd.sh
@@ -2,4 +2,4 @@
 
 wget https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.6.0/pmd-bin-6.6.0.zip
 unzip pmd-bin-6.6.0.zip
-pmd-bin-6.6.0/bin/run.sh pmd -d . -R ruleset.xml -f xml -reportfile pmd-report.xml -l apex
+pmd-bin-6.6.0/bin/run.sh pmd -d . -R apex_ruleset.xml -f xml -reportfile pmd-report.xml -l apex
